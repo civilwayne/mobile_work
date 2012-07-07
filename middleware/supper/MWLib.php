972578a339b9087141f9abfe85b8396ed83ed967
wayne <civilwayne@gmail.com>
2012-07-07 18:33:01 +0800

    smarty singleton

---
+++ middleware/supper/MWLib.php
@@ -0,0 +1,14 @@
+<?php
+
+require( dirname(__file__).'\..\..\lib\server\smarty\Smarty.class.php');
+class MWLib{
+	protected function __construct(){ //can't new again
+		$smarty=  
+		
+	}
+	static function kkikd(){
+		
+	}
+}
+
+?>
